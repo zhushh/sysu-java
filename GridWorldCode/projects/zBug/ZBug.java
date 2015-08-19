714b61534f72aecaee9156051a1822a137048b49
zhushh <zsh5318@gmail.com>
2015-08-19 16:43:45 +0800

    create ZBug

---
+++ GridWorldCode/projects/zBug/ZBug.java
@@ -0,0 +1,71 @@
+/* 
+ * AP(r) Computer Science GridWorld Case Study:
+ * Copyright(c) 2005-2006 Cay S. Horstmann (http://horstmann.com)
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * @author Cay Horstmann
+ * @author Chris Nevison
+ * @author Barbara Cloud Wells
+ */
+
+import info.gridworld.actor.Bug;
+
+/**
+ * A <code>ZBug</code> traces out a square "box" of a given size. <br />
+ * The implementation of this class is testable on the AP CS A and AB exams.
+ */
+public class ZBug extends Bug
+{
+    private int steps;
+    private int sideLength;
+    private int change;
+    private static final int rotation_angle_1 = 135;
+    private static final int rotation_angle_2 = 225;
+
+    /**
+     * Constructs a box bug that traces a square of a given side length
+     * @param length the side length
+     */
+    public ZBug(int length)
+    {
+        steps = 0;
+        sideLength = length;
+        change = 0;
+    }
+
+    /**
+     * Moves to the next location of the square.
+     */
+    public void act()
+    {
+        if (steps < sideLength && canMove())
+        {
+            move();
+            steps++;
+        }
+        else if (steps < sideLength || change >= 2)
+        {
+            // nothing to do
+        }
+        else
+        {
+            int current_direction = getDirection();
+            if (change == 0) {
+                change++;
+                setDirection(current_direction + rotation_angle_1);
+            } else if (change == 1) {
+                change++;
+                setDirection(current_direction + rotation_angle_2);
+            }
+            steps = 0;
+        }
+    }
+}

1c63f19e3509966724a0999d4e60490e5651a827
zhushh <zsh5318@gmail.com>
2015-08-19 09:40:43 +0800

    add ant study file

---
+++ stage1/junitTest/HelloWorld.java
@@ -0,0 +1,16 @@
+// define for junit test
+// using in class HelloWorldTest
+
+import java.util.*;
+
+public class HelloWorld {
+	String str;
+
+	public void hello() {
+		str = "Hello World!";
+	}
+
+	public String getStr() {
+		return str;
+	}
+}
